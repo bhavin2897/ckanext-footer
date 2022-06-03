1fdb713b917cb0711031b7d7022f1fc5834a59b0
Bhavin Katabathuni <bhavin2897@gmail.com>
2022-06-03 16:26:37 +0200

    major - adding IRoute for organization to Repository

--- ckanext/footer/plugin.py
+++ ckanext/footer/plugin.py
@@ -15,6 +15,7 @@ def dataprotection():
 class FooterPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IBlueprint)
+    plugins.implements(plugins.IRoutes, inherit=True)
 
 
     def update_config(self, config_):
@@ -46,3 +47,49 @@ class FooterPlugin(plugins.SingletonPlugin):
             methods=['GET']
         )
         return blueprint
+
+    def before_map(self, map):
+        org_controller = 'ckan.controllers.organization:OrganizationController'
+        with SubMapper(map, controller = org_controller) as m:
+            m.connect('repository_index', '/repository', action='index')
+            m.connect('/repository/list', action='list')
+            m.connect('/repository/new', action='new')
+            m.connect('/repository/{action}/{id}',
+                      requirements=dict(action='|'.join([
+                          'delete',
+                          'admins',
+                          'member_new',
+                          'member_delete',
+                          'history'
+                      ])))
+            m.connect('repository_activity', '/repository/activity/{id}',
+                      action='activity', ckan_icon='time')
+            m.connect('repository_read', '/repository/{id}', action='read')
+            m.connect('repository_about', '/repository/about/{id}',
+                      action='about', ckan_icon='info-sign')
+            m.connect('repository_read', '/repository/{id}', action='read',
+                      ckan_icon='sitemap')
+            m.connect('repository_edit', '/repository/edit/{id}',
+                      action='edit', ckan_icon='edit')
+            m.connect('repository_members', '/repository/edit_members/{id}',
+                      action='members', ckan_icon='group')
+            m.connect('repository_bulk_process',
+                      '/repository/bulk_process/{id}',
+                      action='bulk_process', ckan_icon='sitemap')
+
+        map.redirect('/api/{ver:1|2|3}/rest/repository',
+                     '/api/{ver}/rest/group')
+        map.redirect('/api/rest/repository', '/api/rest/group')
+        map.redirect('/api/{ver:1|2|3}/rest/repository/{url:.*}',
+                     '/api/{ver}/rest/group/{url:.*}')
+        map.redirect('/api/rest/repository/{url:.*}',
+                     '/api/rest/group/{url:.*}')
+
+        map.connect('repository_members_read', '/repository/members/{id}',
+                    controller='ckanext.iati.controllers.repository:repositoryController',
+                    action='members_read', ckan_icon='group')
+
+        return map
+
+    def after_map(self,map):
+        return map
