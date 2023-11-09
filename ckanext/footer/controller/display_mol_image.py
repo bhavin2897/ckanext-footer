d9cbdad4af633b7a51fa5039830bee786485254b
Bhavin Katabathuni <bhavin2897@gmail.com>
2023-11-09 11:51:00 +0100

    MoleculeView page displaying mol_formula, name & weight

--- ckanext/footer/controller/display_mol_image.py
+++ ckanext/footer/controller/display_mol_image.py
@@ -3,6 +3,8 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 from flask import Blueprint, render_template, session
 
+import requests
+
 import logging
 import json
 from typing import Any, Dict
@@ -13,6 +15,12 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from PIL import Image
 import io
 import base64
+
+from rdkit.Chem import inchi
+from rdkit.Chem import rdmolfiles
+from rdkit.Chem import Draw
+from rdkit.Chem import Descriptors
+from rdkit.Chem import rdMolDescriptors
 
 log = logging.getLogger(__name__)
 
@@ -32,6 +40,8 @@ class FooterController(plugins.SingletonPlugin):
     def display_search_mol_image(package_inchiKey, page):
         inchi_key = package_inchiKey
 
+
+
         filepath = '/var/lib/ckan/default/storage/images/' + str(inchi_key) + '.png'
         file = open(filepath, 'rb').read()
         image = Image.open(io.BytesIO(file))
@@ -44,9 +54,18 @@ class FooterController(plugins.SingletonPlugin):
         session['byteimage'] = byteimage
         session['page'] = page
 
-        return byteimage
+        # Get Molecular Formula using PubChem
+        r = requests.get(
+            f'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/inchikey/{inchi_key}/property/MolecularFormula,IUPACName/JSON').json()
+        mol_formula = r['PropertyTable']['Properties'][0]['MolecularFormula']
+
+        # Get IUPAC name using PubChem
+        iupacName = r['PropertyTable']['Properties'][0]['IUPACName']
+
+        return byteimage, mol_formula,iupacName
 
     def searchbar():
         byte_image = session.get('byteimage', None)
         page = session.get('page', None)
-        return render_template('search_bar/search_bar.html', bytename=byte_image, page=page)
+
+        return render_template('search_bar/search_bar.html', bytename=byte_image, page=page, )
