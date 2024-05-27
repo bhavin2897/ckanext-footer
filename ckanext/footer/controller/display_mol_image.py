238c0f715d2ba3fe36d34c451fcbd0f16ab88c9d
Bhavin Katabathuni <bhavin2897@gmail.com>
2024-05-27 14:58:23 +0200

    Facets list to show

--- ckanext/footer/controller/display_mol_image.py
+++ ckanext/footer/controller/display_mol_image.py
@@ -6,20 +6,17 @@ import ckan.model as model
 from ckanext.rdkit_visuals.models.molecule_rel import MolecularRelationData as mol_relation_data
 
 from flask import Blueprint, render_template, session
-
+from ckanext.footer.controller.search_controller import SearchMoleculeController
 import requests
 import math
-
 
 import logging
 import json
 from typing import Any, Dict
 
-
 from PIL import Image
 import io
 import base64
-
 
 log = logging.getLogger(__name__)
 
@@ -29,6 +26,14 @@ class FooterController(plugins.SingletonPlugin):
 
     @staticmethod
     def display_search_mol_image(package_inchiKey, page):
+        """
+        Function to generate image in bytes code, which is later converted to an image, using the packageID
+        Similar to the RDKit Visuals extension!!
+
+        :param package_inchiKey: Receives InChIKey from the HTML which is an advanced Search & an exact Match
+        :param page: page number we are in(starting from 0)
+        :return: Either returns image in the format of bytecode or NONE
+        """
 
         inchi_key = package_inchiKey
 
@@ -56,19 +61,25 @@ class FooterController(plugins.SingletonPlugin):
             log.debug(f"Error in display_search_mol_image {e}")
 
     def get_molecule_data(package_id):
+        """
+        generates molecular data, that is displayed besides the molecule image, for the particular molecule
+
+        :param package_id: receives package id from
+        :return: molecule formal, exact mass and inchi are returned to display beside the image
+        """
 
         mol_formula = []
         inchi_n = []
-        #package_id_show = package_id
+        # package_id_show = package_id
 
         molecule_formula_list = mol_relation_data.get_mol_formula_by_package_id(package_id)
         exact_mass_list = mol_relation_data.get_exact_mass_by_package_id(package_id)
 
         inchi = mol_relation_data.get_molecule_data_by_package_id(package_id)
 
-        inchi_n = inchi[0][0].replace('["','').replace('"]','')
+        inchi_n = inchi[0][0].replace('["', '').replace('"]', '')
 
-        #log.debug(inchi_n)
+        # log.debug(inchi_n)
         try:
             for x in molecule_formula_list:
                 mol_formula = "['']".join(x)
@@ -77,19 +88,26 @@ class FooterController(plugins.SingletonPlugin):
                 return mol_formula, exact_mass, inchi_n
 
         except TypeError:
-            return None, None,None
-
-
-
+            return None, None, None
 
     def searchbar():
+        """
+        to render data to HTML using flask blueprint
+        :return: render_template of Flask.blueprint
+        """
         byte_image = session.get('byteimage', None)
         page = session.get('page', None)
 
         return render_template('search_bar/search_bar.html', bytename=byte_image, page=page, )
 
-
     def mol_dataset_list():
+        """
+        Gets the numbers of the page, and sends to the 'get_package_list' alembic method,
+        to return the list of InChIKey & packageID pair
+
+        :return: list of packages paired [InChIKey, packageID], along with 'current page', 'total pages' and
+        'total datasets'.
+        """
         page = toolkit.request.args.get('page', 1, type=int)
         current_page = page
         page_size = 10
@@ -98,14 +116,18 @@ class FooterController(plugins.SingletonPlugin):
 
         total_datasets = mol_relation_data.get_count_rows()
         total_pages = math.ceil(total_datasets / page_size)
-        
+
         return package_list_inchi_key, current_page, total_pages, total_datasets
 
-
-
     def package_show_dict(package_ids):
+        """
+        List of all packages/datasets
+        :return:
+        """
 
         package_list_for_every_inchi = []
+        facet_field_list = []
+
         try:
             if package_ids:
                 package_ids_list = [package_ids]
@@ -113,9 +135,25 @@ class FooterController(plugins.SingletonPlugin):
                 for package_id in package_ids_list:
                     package = toolkit.get_action('package_show')({}, {'name_or_id': package_id})
                     package_list_for_every_inchi.append(package)
-                    # log.debug(f'{package_list_for_every_inchi}')
+
         except Exception as e:
-            log.debug(e)
+            log.exception(e)
 
         return package_list_for_every_inchi
 
+
+    def get_facet_field_list():
+        """
+        Get all Facets Field in PackageSearch all!!
+
+        :return:
+        """
+
+        facet_field_list = []
+        facets_per_dataset = toolkit.get_action('package_search')({}, {'fq': '', 'facet.field': ['tags', 'organization',
+                                                                                                 'measurement_technique',
+                                                                                                 'license_id']})
+
+        facet_field_list.append(facets_per_dataset)
+
+        return facet_field_list
--- ckanext/footer/plugin.py
+++ ckanext/footer/plugin.py
@@ -9,13 +9,14 @@ from flask import Blueprint, render_template, session
 import asyncio
 from ckanext.footer.controller.display_mol_image import FooterController
 
+
 import logging
 import json
 from typing import Any, Dict
 
 log = logging.getLogger(__name__)
-get_action = logic.get_action
 
+get_action = logic.get_action
 
 
 def help():
@@ -31,7 +32,7 @@ def dataprotection():
 
 
 def molecule_view():
-    return render_template('molecule_view.html')
+    return render_template('molecule_view/molecule_view.html')
 
 
 class FooterPlugin(plugins.SingletonPlugin):
@@ -40,17 +41,7 @@ class FooterPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.ITemplateHelpers)
     plugins.implements(plugins.IFacets, inherit=True)
     plugins.implements(plugins.IPackageController, inherit=True)
-
-
-# Facet plugin
-    def dataset_facets(self,facets_dict,package_type):
-        facets_dict = {
-            'organization': 'Repository',
-            'measurement_technique': 'Measurement Technique',
-            'res_format':'Formats',
-            'license_id': 'Licenses',
-        }
-        return facets_dict
+    plugins.implements(plugins.IRoutes,inherit=True)
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
@@ -109,14 +100,21 @@ class FooterPlugin(plugins.SingletonPlugin):
                 'mol_package_list': FooterController.mol_dataset_list,
                 'package_list_for_every_inchi': FooterController.package_show_dict,
                 'get_molecule_data': FooterController.get_molecule_data,
-                'package_list': FooterPlugin.molecule_view_search, }
+                'package_list': FooterPlugin.molecule_view_search,
+                'get_facet_field_list': FooterController.get_facet_field_list}
 
 
+    @staticmethod
+    def before_search(search_params: dict[str, Any]) -> dict[str, Any]:
+
+        session['search_params'] = search_params
+
+        return search_params
 
     @staticmethod
     def after_search(search_results: dict[str, Any], search_params: dict[str, Any]) -> dict[str, Any]:
 
-        search_params = search_params.copy()
+        # search_params = search_params.copy()
 
         if search_params['q'] == '*:*':
             search_params_result = None
@@ -125,13 +123,25 @@ class FooterPlugin(plugins.SingletonPlugin):
 
         session['search_results_final'] = search_results
         session['search_params'] = search_params_result
+
+
         return search_results
 
     def molecule_view_search():
         packages_list = session.get('search_results_final', None)
         search_params = session.get('search_params', None)
+
         return packages_list, search_params
 
+    # IRoutes implementation
+    def before_map(self, map):
+        # Redirect from /dataset to /molecule_view
+        map.redirect('/dataset', '/molecule_view', _redirect_code='301')
+        # Ensure /molecule_view is handled by a specific controller or function
+        map.connect('molecule_view',
+                    '/molecule_view',
+                    controller= FooterController ,
+                    action='handle_molecule_view',
+                    conditions=dict(method=['GET']))
 
-
-
+        return map
